628f0e2e559b95367386ae6145521b1b8d4c84b8
SDanishHassan <shazadadanishhassan@gmail.com>
2022-02-28 15:23:53 +0530

    Foter styling

--- src/features/common/FooterSection.js
+++ src/features/common/FooterSection.js
@@ -5,22 +5,22 @@ class FooterSection extends React.PureComponent{
 
     return(
         <div className='section-padding'>
-            <div >
+            <div  className='items-center'>
 
               <img className='logo-size' src='./images/logo.png' alt='logo'></img>
             </div>
 
-            <div className='mt10'>
+            <div className='mt10 items-center'>
               <span className='bold-text'>Follow Us</span>
             </div>
 
 
-            <div className='mt10'>
+            <div className='mt10 items-center'>
               <img className='logo-size' src='./images/fb.png' alt='img'></img>
               <img className='logo-size' src='./images/insta.png' alt='img'></img>
             </div>
 
-            <div className='mt20'>
+            <div className='mt20 items-center'>
               <span>Copyright ©2022 All rights reserved | This website is made with  by <span className='red-text'>Bracecodes Software Solutions Pvt. Ltd.</span></span>
             </div>
 
